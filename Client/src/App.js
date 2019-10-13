a76aae45dec672943a7be912483bea0170081d28
Pellizzon <matheusp1@al.insper.edu.br>
2019-10-13 14:27:53 -0300

    long medium short

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import "./static/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import SpotifyWebApi from 'spotify-web-api-js';
-import request from 'request';
-import standard from './static/standard.png';
+import SpotifyWebApi from "spotify-web-api-js";
+import request from "request";
+import standard from "./static/standard.png";
 
 var spotifyApi = new SpotifyWebApi();
 
 function getHashParams() {
   var hashParams = {};
-  var e, r = /([^&;=]+)=?([^&;]*)/g,
+  var e,
+    r = /([^&;=]+)=?([^&;]*)/g,
     q = window.location.hash.substring(1);
-  e = r.exec(q)
+  e = r.exec(q);
   while (e) {
     hashParams[e[1]] = decodeURIComponent(e[2]);
     e = r.exec(q);
@@ -21,47 +22,40 @@ function getHashParams() {
 }
 
 function App() {
-
   const token = getHashParams().access_token;
   if (token) {
     spotifyApi.setAccessToken(token);
   }
   const [user, setUser] = useState();
   const [playlist, setPlaylist] = useState();
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [myFestival, setMyFestival] = useState();
+  const [term, setTerm] = useState();
 
   //User information
   useEffect(() => {
-    const getUser = async () => {
-      spotifyApi.getMe().then((data) => {
-        setUser(data);
-      })
-    }
-    const token = spotifyApi.getAccessToken();
-    if (token) {
-      getUser();
-    }
+    spotifyApi.getMe().then(data => {
+      setUser(data);
+    });
   }, []);
 
   //Playlist information
   useEffect(() => {
-    const getPlaylist = async () => {
-      var options = {
-        url: 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50',
-        headers: { 'Authorization': 'Bearer ' + spotifyApi.getAccessToken() },
-        json: true
-      };
-
-      request.get(options, (error, response, body) => {
-        setPlaylist(body.items);
-      });
-    }
-    const token = spotifyApi.getAccessToken();
-    if (token) {
-      getPlaylist();
-    }
+    getTerm("short_term");
   }, []);
+
+  const getTerm = e => {
+    var options = {
+      url: `https://api.spotify.com/v1/me/top/tracks?time_range=${e}&limit=50`,
+      headers: { Authorization: "Bearer " + spotifyApi.getAccessToken() },
+      json: true
+    };
+
+    request.get(options, (error, response, body) => {
+      setPlaylist(body.items);
+      setTerm(e);
+    });
+  };
 
   if (playlist && user && token) {
     for (var i = 0; i < playlist.length; i++) {
@@ -69,71 +63,120 @@ function App() {
       playlist[i]["ranking"] = ranking;
     }
     const filteredData = playlist.filter(item => {
-        return item.name.toLowerCase().includes(search.toLowerCase())
+      return item.name.toLowerCase().includes(search.toLowerCase());
     });
 
     const createPlaylist = () => {
-      spotifyApi.getUserPlaylists(user.id).then((data) => {
-        for (var i =0; i < data.items.length; i++) {
-          if (data.items[i].name !== 'myFestival') {
-            setMyFestival("notFound")
+      spotifyApi.getUserPlaylists(user.id).then(data => {
+        for (var i = 0; i < data.items.length; i++) {
+          if (data.items[i].name !== "myFestival") {
+            setMyFestival("notFound");
           } else {
-            setMyFestival(data.items[i].id)
-            var l = []
+            setMyFestival(data.items[i].id);
+            var l = [];
             for (var j = 0; j < playlist.length; j++) {
-              l.push(playlist[j].uri)
+              l.push(playlist[j].uri);
             }
-            spotifyApi.addTracksToPlaylist(data.items[i].id, l)
-            break
+            spotifyApi.addTracksToPlaylist(data.items[i].id, l);
+            break;
           }
         }
-      })
-      
+      });
     };
 
     return (
       <div className="app">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
         <div>
           <Header title="My Festival" />
         </div>
         <span className="user">
-          {typeof (user.images[0]) !== "undefined" && (
-            <img src={user.images[0].url} className='user-img' width="60" height="60" alt='user pic' />
+          {typeof user.images[0] !== "undefined" && (
+            <img
+              src={user.images[0].url}
+              className="user-img"
+              width="60"
+              height="60"
+              alt="user pic"
+            />
           )}
-          {typeof (user.images[0]) === "undefined" && (
-            <img src={standard} className='user-img' width="60" height="60" alt='user pic' />
+          {typeof user.images[0] === "undefined" && (
+            <img
+              src={standard}
+              className="user-img"
+              width="60"
+              height="60"
+              alt="user pic"
+            />
           )}
           <div className="user-infos">
             <p>Logged in as {user.display_name}</p>
             <p>{user.email}</p>
           </div>
         </span>
-        <p><a href='localhost:3000' className="link logout" >Logout</a></p>
-        <p><button onClick={() => createPlaylist()}>Create My Festival Playlist!</button></p>
-        {typeof (myFestival) !== "undefined"  && myFestival !== "notFound" && (
-            <p>Playlist criada</p>
-          )
-        }
-        {myFestival === "notFound" && (
-          <p>playlist não encontrada</p>
+        <p>
+          <a href="localhost:3000" className="link logout">
+            Logout
+          </a>
+        </p>
+        {term === "short_term" && (
+          <p>
+            <button onClick={() => getTerm("medium_term")}>Medium Term</button>
+            <button onClick={() => getTerm("long_term")}>Long Term</button>
+          </p>
         )}
-        <input className="search" type="text" onChange={e => setSearch(e.target.value)} placeholder="&#xF002; Search Track Name..." value={search}/>
-        <div className="list">{filteredData.map((obj, index) => {
-          return (
-            <div className="cartao" key={index}>
-              <h1>{obj.ranking}</h1>
-              <div className="conteudo">
-                <p>Track: {obj.name}</p>
-                <p>Artists: {obj.artists[0].name}</p>
-                <p>Album: {obj.album.name}</p>
+        {term === "medium_term" && (
+          <p>
+            <button onClick={() => getTerm("short_term")}>Short Term</button>
+            <button onClick={() => getTerm("long_term")}>Long Term</button>
+          </p>
+        )}
+        {term === "long_term" && (
+          <p>
+            <button onClick={() => getTerm("short_term")}>Short Term</button>
+            <button onClick={() => getTerm("medium_term")}>Medium Term</button>
+          </p>
+        )}
+        <p>
+          <button onClick={() => createPlaylist()}>
+            Create My Festival Playlist!
+          </button>
+        </p>
+        {typeof myFestival !== "undefined" && myFestival !== "notFound" && (
+          <p>Created myFestival!</p>
+        )}
+        {myFestival === "notFound" && <p>Playlist not found...</p>}
+        <input
+          className="search"
+          type="text"
+          onChange={e => setSearch(e.target.value)}
+          placeholder="&#xF002; Search Track Name..."
+          value={search}
+        />
+        <div className="list">
+          {filteredData.map((obj, index) => {
+            return (
+              <div className="cartao" key={index}>
+                <h1>{obj.ranking}</h1>
+                <div className="conteudo">
+                  <p>Track: {obj.name}</p>
+                  <p>Artists: {obj.artists[0].name}</p>
+                  <p>Album: {obj.album.name}</p>
+                </div>
+                <img
+                  src={obj.album.images[0].url}
+                  width="60"
+                  height="60"
+                  alt="album pic"
+                />
               </div>
-              <img src={obj.album.images[0].url} width="60" height="60" alt="album pic" />
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
-      </div >
+      </div>
     );
   }
 
@@ -143,9 +186,11 @@ function App() {
         <Header title="My Festival" />
       </div>
       <span className="user">
-        <a href='localhost:8888/login' className="link" >Login with Spotify</a>
+        <a href="localhost:8888/login" className="link">
+          Login with Spotify
+        </a>
       </span>
-    </div >
+    </div>
   );
 }
 
